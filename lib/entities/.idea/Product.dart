d48f56c953de781d55a1853c62d14768997f9f7d
TranDangVietHuy <viethuy2904@gmail.com>
2026-05-19 22:13:21 +0700

    add PRM393 lab exercise

---
+++ lib/entities/.idea/Product.dart
@@ -0,0 +1,19 @@
+/**LAB 1
+ * Xây dựng lớp Product với các thuộc tính id, name, price, image
+ * có các thuộc tính va fdanh sách tĩnh gồm List<Product> products
+ * ( gán cho 1 số các giá trị mặc định) và xây dựng các phương thức sau:
+ *   - Phương thức khởi tạo, phương thức factory để map một Json về Product
+ *   - Phương thức add(product), edit(product), search(name),
+ *   increasePrice(tăng giá mỗi sp lên 10%), dùng decarative-map)
+ *   find,...
+ *  để hiển thi, sắp xếp, thêm, xóa sửa , tìm kiếm
+ */
+
+class Product {
+  final int id;
+  final String name
+  final double price;
+  final String image;
+
+  Product({})
+}
